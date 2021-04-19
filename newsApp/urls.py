1c0f2b76e1c26a606f0c5be82814259b00fcc31f
Deepak50 <deepakkushalappa123@gmail.com>
2021-04-19 23:18:48 +0530

    f commit

--- newsApp/urls.py
+++ newsApp/urls.py
@@ -6,10 +6,18 @@ from . import views   #from local directory
 
 urlpatterns = [
     path('',views.index),
-    path('readmore',views.readmore, name = "readmore"),
-    path('save',views.save, name = "save"),
-    path('delete',views.delete, name = "delete"),
+    path('readmore', views.readmore, name = "readmore"),
+    path('save', views.save, name = "save"),
+    path('delete', views.delete, name = "delete"),
+    path('sports', views.sports, name = "sports"),
+    path('science', views.science, name = "science"),
+    path('india', views.india, name = "india"),
+    path('entertainment', views.entertainment, name = "entertainment"),
+    path('us', views.us, name = "us"),
+    path('health', views.health, name = "health"),
+    path('world', views.world, name = "world"),
 ]
+
 
 
 # from django.conf.urls import url
--- newsApp/views.py
+++ newsApp/views.py
@@ -7,8 +7,8 @@ from django.http import HttpResponse
 mylist = []
 def index(request): 
     Post.objects.all().delete()
-    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')
-    headLines = newsApi.get_top_headlines(sources = 'bbc-news, the-verge')
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines( sources="bbc-news, the-verge")  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
     articles = headLines['articles']
     
     desc = []
@@ -59,3 +59,167 @@ def save(request):
 def delete(request):
     Saved.objects.all().delete()
     return render(request, "main/delete.html")
+
+
+def sports(request):
+    Post.objects.all().delete()
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines( category = 'sports')  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
+    articles = headLines['articles']
+    
+    desc = []
+    news = []
+    img = []
+    idd = []
+    
+    for i in range(len(articles)):
+        article = articles[i]
+        idd.append(i)
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+        Post.objects.create(idd = i, desc = desc[i], news = news[i], img = img[i])
+    mylist = zip(idd, news, desc, img)
+    return render(request, "main/index.html", context = {"mylist": mylist})
+
+def sports(request):
+    Post.objects.all().delete()
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines(category="sports", country="in")  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
+    articles = headLines['articles']
+    desc = []
+    news = []
+    img = []
+    idd = []
+    
+    for i in range(len(articles)):
+        article = articles[i]
+        idd.append(i)
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+        Post.objects.create(idd = i, desc = desc[i], news = news[i], img = img[i])
+    mylist = zip(idd, news, desc, img)
+    return render(request, "main/index.html", context = {"mylist": mylist})
+
+def science(request):
+    Post.objects.all().delete()
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines(category = "science")  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
+    articles = headLines['articles']
+    desc = []
+    news = []
+    img = []
+    idd = []
+    
+    for i in range(len(articles)):
+        article = articles[i]
+        idd.append(i)
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+        Post.objects.create(idd = i, desc = desc[i], news = news[i], img = img[i])
+    mylist = zip(idd, news, desc, img)
+    return render(request, "main/index.html", context = {"mylist": mylist})
+
+def india(request):
+    Post.objects.all().delete()
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines(sources="the-times-of-india")  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
+    articles = headLines['articles']
+    desc = []
+    news = []
+    img = []
+    idd = []
+    
+    for i in range(len(articles)):
+        article = articles[i]
+        idd.append(i)
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+        Post.objects.create(idd = i, desc = desc[i], news = news[i], img = img[i])
+    mylist = zip(idd, news, desc, img)
+    return render(request, "main/index.html", context = {"mylist": mylist})
+
+def entertainment(request):
+    Post.objects.all().delete()
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines(category="entertainment", country="in")  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
+    articles = headLines['articles']
+    desc = []
+    news = []
+    img = []
+    idd = []
+    
+    for i in range(len(articles)):
+        article = articles[i]
+        idd.append(i)
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+        Post.objects.create(idd = i, desc = desc[i], news = news[i], img = img[i])
+    mylist = zip(idd, news, desc, img)
+    return render(request, "main/index.html", context = {"mylist": mylist})
+
+def us(request):
+    Post.objects.all().delete()
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines(country="us")  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
+    articles = headLines['articles']
+    desc = []
+    news = []
+    img = []
+    idd = []
+    
+    for i in range(len(articles)):
+        article = articles[i]
+        idd.append(i)
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+        Post.objects.create(idd = i, desc = desc[i], news = news[i], img = img[i])
+    mylist = zip(idd, news, desc, img)
+    return render(request, "main/index.html", context = {"mylist": mylist})
+
+
+def health(request):
+    Post.objects.all().delete()
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines(category="health")  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
+    articles = headLines['articles']
+    desc = []
+    news = []
+    img = []
+    idd = []
+    
+    for i in range(len(articles)):
+        article = articles[i]
+        idd.append(i)
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+        Post.objects.create(idd = i, desc = desc[i], news = news[i], img = img[i])
+    mylist = zip(idd, news, desc, img)
+    return render(request, "main/index.html", context = {"mylist": mylist})
+
+def world(request):
+    Post.objects.all().delete()
+    newsApi = NewsApiClient(api_key = '508840f478804c84a86421ab5394a080')  #https://newsapi.org/v2/top-headlines?country=in&apiKey=508840f478804c84a86421ab5394a080
+    headLines = newsApi.get_top_headlines(sources="bbc-news, the-verge")  #bbc-news, the-verge  sources = 'the-times-of-india, google-news',
+    articles = headLines['articles']
+    desc = []
+    news = []
+    img = []
+    idd = []
+    
+    for i in range(len(articles)):
+        article = articles[i]
+        idd.append(i)
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+        Post.objects.create(idd = i, desc = desc[i], news = news[i], img = img[i])
+    mylist = zip(idd, news, desc, img)
+    return render(request, "main/index.html", context = {"mylist": mylist})
+
